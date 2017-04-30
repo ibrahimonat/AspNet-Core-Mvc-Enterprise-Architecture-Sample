c9e3f7b81b9f155ae305406aacb71ef91a51c31e
İbrahim Onat <onat.ibrahim@gmail.com>
2017-04-30 23:55:35 +0300

    Added some entities.

---
+++ Ontsoft.Northwind.Entities/Concrete/Language.cs
@@ -0,0 +1,10 @@
+﻿using Ontsoft.Core.Entities;
+
+namespace Ontsoft.Northwind.Entities.Concrete
+{
+    public class Language : IEntity
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ Ontsoft.Northwind.Entities/Concrete/LanguageWord.cs
@@ -0,0 +1,11 @@
+﻿using Ontsoft.Core.Entities;
+
+namespace Ontsoft.Northwind.Entities.Concrete
+{
+   public class LanguageWord : IEntity
+    {
+        public int LanguageId { get; set; }
+        public string Code { get; set; }
+        public string Value { get; set; }
+    }
+}
